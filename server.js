4cd6b1092f5535cddbefa1d9118fc5aef8f9727b
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-08-22 00:00:10 +0300

    Tanımlar

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use('/api/seyahat/diger-talepler', digerRoutes);
 const talepTipiRoutes = require('./routes/talepTipi.routes');
 app.use('/api/talep-tipleri', talepTipiRoutes);
 const misafirTalepRoutes = require("./routes/misafirTalep.routes");
-app.use("/api/seyahat/misafir-talepler", misafirTalepRoutes);
+app.use("/api/misafir-talep", misafirTalepRoutes);
 
 
 // 🧠 MongoDB Bağlantısı
