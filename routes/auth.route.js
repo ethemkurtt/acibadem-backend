40795733fc2c87798a09d6da7ba919ceb92645b4
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-09-13 13:40:14 +0300

    Arac atama

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -59,7 +59,7 @@ router.use("/users", authenticate);
 router.get("/users", userController.getAllUsers);
 router.get("/users/:id", userController.getUserById);
 router.put("/users/:id", userController.updateUser);
-router.delete("/users/:id", userController.deleteUser);
+// router.delete("/users/:id", userController.deleteUser);
 router.post("/users", userController.createUser);
 router.get("/soforler",  userController.getSoforler);
 // Sadece superadmin erişimi olan örnek endpoint
