b1a8c0eecbf9011e87470b2cb46a4ae85a0f822a
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-10-07 23:48:16 +0300

    Talepler

--- routes/hastaTalepDetay.routes.js
+++ routes/hastaTalepDetay.routes.js
@@ -5,9 +5,6 @@ const ctrl = require("../controllers/hastaTalepDetay.controller");
 
 // Tek başına detay CRUD
 router.post("/", ctrl.createCombined);
-router.get("/by-talep/:talepId", ctrl.getByTalepId);
-router.put("/by-talep/:talepId", ctrl.updateByTalepId);
-router.delete("/by-talep/:talepId", ctrl.deleteByTalepId);
 
 // Birleştirilmiş (talepler + detay) tek endpoint
 router.post("/combined", ctrl.createCombined);
