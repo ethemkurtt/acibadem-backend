4be2535ee95ac1ef669de6707ffa7b40290e8707
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-10-08 16:40:29 +0300

    Talepler

--- routes/hastaTalepDetay.routes.js
+++ routes/hastaTalepDetay.routes.js
@@ -8,7 +8,7 @@ router.post("/", ctrl.createCombined);
 // router.get("/by-talep/:talepId", ctrl.getByTalepId);
 // router.put("/by-talep/:talepId", ctrl.updateByTalepId);
 // router.delete("/by-talep/:talepId", ctrl.deleteByTalepId);
-
+router.put("/combined/:talepId", validateObjectId("talepId"), ctrl.updateCombined);
 // Birleştirilmiş (talepler + detay) tek endpoint
 router.post("/combined", ctrl.createCombined);
 
