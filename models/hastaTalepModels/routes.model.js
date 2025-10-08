e1d46e5e39765696ad30e129aeeab57ddd702a40
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-10-08 21:47:54 +0300

    Talepler

--- models/hastaTalepModels/routes.model.js
+++ models/hastaTalepModels/routes.model.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
 const FileSchema = new mongoose.Schema({
-  fileName: String,
-  filePath: String,
+  fileName: { type: String, default: null },
+  filePath: { type: String, default: null },
 });
 
 const RoutesSchema = new mongoose.Schema({
-  hastaId: { type: mongoose.Schema.Types.ObjectId, ref: "HastaTalep" },
+  hastaId: { type: mongoose.Schema.Types.ObjectId, ref: "HastaTalep", default: null },
+
   pickup: {
     type: {
       type: String,
+      default: null,
     },
     locationId: {
       type: mongoose.Schema.Types.ObjectId,
@@ -17,16 +19,15 @@ const RoutesSchema = new mongoose.Schema({
       default: null,
     },
     locationName: { type: String, default: null },
-    date: Date,
-    person: Number,
-    baggage: Number,
-    flightCode: String,
-    departure: Date,
-    arrival: Date,
-    ticket: String,
-    passport: String,
+    date: { type: Date, default: null },
+    person: { type: Number, default: null },
+    baggage: { type: Number, default: null },
+    flightCode: { type: String, default: null },
+    departure: { type: Date, default: null },
+    arrival: { type: Date, default: null },
+    ticket: { type: String, default: null },
+    passport: { type: String, default: null },
 
-    // 🔹 Yeni eklenen alanlar
     il_adi: { type: String, default: null },
     pickupManuelLocation: { type: String, default: null },
     ulke_kodu: { type: String, default: "TR" },
@@ -34,27 +35,27 @@ const RoutesSchema = new mongoose.Schema({
     il_kodu: { type: String, default: null },
     ilce_kodu: { type: String, default: null },
   },
+
   drop: {
     type: {
       type: String,
+      default: null,
     },
     locationId: {
       type: mongoose.Schema.Types.ObjectId,
       refPath: "drop.type",
       default: null,
     },
-
     locationName: { type: String, default: null },
-    date: Date,
-    person: Number,
-    baggage: Number,
-    flightCode: String,
-    departure: Date,
-    arrival: Date,
-    ticket: String,
-    passport: String,
+    date: { type: Date, default: null },
+    person: { type: Number, default: null },
+    baggage: { type: Number, default: null },
+    flightCode: { type: String, default: null },
+    departure: { type: Date, default: null },
+    arrival: { type: Date, default: null },
+    ticket: { type: String, default: null },
+    passport: { type: String, default: null },
 
-    // 🔹 Yeni eklenen alanlar
     il_adi: { type: String, default: null },
     dropManuelLocation: { type: String, default: null },
     ulke_kodu: { type: String, default: "TR" },
