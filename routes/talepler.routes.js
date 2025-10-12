e8d1b8a962884deafd8e8da8414d635a75d07a44
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-10-12 12:27:34 +0300

    Talepler

--- routes/talepler.routes.js
+++ routes/talepler.routes.js
@@ -3,18 +3,21 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers/talepler.controller");
 const { authRequired } = require("../middlewares/auth");
-router.get("/detail/:id", ctrl.getFullById);   // <- YENİ
-// Ortak talepler
+
+// Spesifikler önce
+router.get("/detail/:id", ctrl.getFullById);
 
 router.post("/", ctrl.create);
 router.get("/", ctrl.list);
-router.get("/aracTalep", authRequired,ctrl.aracTalep);
-router.get("/taleplerim", authRequired,ctrl.taleplerim);
-router.get("/aracIsEmri", authRequired,ctrl.aracIsEmri);
-router.get("/:id", ctrl.getById);
 
-router.put("/:id", ctrl.updateById);
-router.put("/:id/atama", authRequired,ctrl.assignAracSofor);
-router.delete("/:id", ctrl.deleteById);
+router.get("/aracTalep", authRequired, ctrl.aracTalep);
+router.get("/taleplerim", authRequired, ctrl.taleplerim);
+router.get("/aracIsEmri", authRequired, ctrl.aracIsEmri);
+
+// Parametreli rotaları isimlendir
+router.get("/id/:id", ctrl.getById);
+router.put("/id/:id", ctrl.updateById);
+router.put("/id/:id/atama", authRequired, ctrl.assignAracSofor);
+router.delete("/id/:id", ctrl.deleteById);
 
 module.exports = router;
