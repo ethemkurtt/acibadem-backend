7ada8354d43710fe38216a173e7f2f728e2e9b5f
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-10-12 13:16:08 +0300

    Talepler

--- routes/talepler.routes.js
+++ routes/talepler.routes.js
@@ -4,20 +4,20 @@ const router = express.Router();
 const ctrl = require("../controllers/talepler.controller");
 const { authRequired } = require("../middlewares/auth");
 
-// Spesifikler önce
-router.get("/detail/:id", ctrl.getFullById);
+// Spesifik yollar her zaman önce
+router.get("/detail/:id([0-9a-fA-F]{24})", ctrl.getFullById);
 
 router.post("/", ctrl.create);
 router.get("/", ctrl.list);
 
-router.get("/aracTalep", authRequired, ctrl.aracTalep);
-router.get("/taleplerim", authRequired, ctrl.taleplerim);
-router.get("/aracIsEmri", authRequired, ctrl.aracIsEmri);
+router.get("/aracTalep",   authRequired, ctrl.aracTalep);
+router.get("/taleplerim",  authRequired, ctrl.taleplerim);
+router.get("/aracIsEmri",  authRequired, ctrl.aracIsEmri);
 
-// Parametreli rotaları isimlendir
-router.get("/id/:id", ctrl.getById);
-router.put("/id/:id", ctrl.updateById);
-router.put("/id/:id/atama", authRequired, ctrl.assignAracSofor);
-router.delete("/id/:id", ctrl.deleteById);
+// Yalnızca geçerli ObjectId’ler eşleşir
+router.get("/:id([0-9a-fA-F]{24})", ctrl.getById);
+router.put("/:id([0-9a-fA-F]{24})", ctrl.updateById);
+router.put("/:id([0-9a-fA-F]{24})/atama", authRequired, ctrl.assignAracSofor);
+router.delete("/:id([0-9a-fA-F]{24})", ctrl.deleteById);
 
 module.exports = router;
