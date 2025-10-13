23d789fc4740f5fe2754c0f2ad4153fa9bf11335
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-10-14 00:59:06 +0300

    Chat

--- models/hastaTalepModels/notificationPerson.model.js
+++ models/hastaTalepModels/notificationPerson.model.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-const NotificationPersonSchema = new mongoose.Schema({
-  hastaId: { type: mongoose.Schema.Types.ObjectId, ref: "HastaTalep" },
-  fullName: { type: String, required: true },
-  description: { type: String }
-}, { timestamps: true });
+const NotificationPersonSchema = new mongoose.Schema(
+  {
+    hastaId: { type: mongoose.Schema.Types.ObjectId, ref: "HastaTalep" },
+    fullName: { type: String, default: null },
+    description: { type: String, default: null },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("NotificationPerson", NotificationPersonSchema);
