949fcaed3b986a8c402a5915b7c674cb011a8079
ethemkurtt <109578812+ethemkurtt@users.noreply.github.com>
2025-10-13 12:59:21 +0300

    Chat

--- models/hastane/hastane.model.js
+++ models/hastane/hastane.model.js
@@ -1,15 +1,17 @@
 // models/Hastane.js
 const mongoose = require("mongoose");
 
-const hastaneSchema = new mongoose.Schema({
-  lokasyon: { type: String, required: true },
-  adres: { type: String, required: true },
+const hastaneSchema = new mongoose.Schema(
+  {
+    lokasyon: { type: String, required: true },
+    adres: { type: String, required: true },
 
-  // Şehir bilgileri
-  il_kodu:   { type: String,default:"" },
-  ilce_kodu: { type: String,default:""}      
-}, { timestamps: true });
-
-
+    // Şehir bilgileri
+    il_kodu: { type: String, default: "" },
+    ilce_kodu: { type: String, default: "" },
+    kordinat: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Hastane", hastaneSchema);
